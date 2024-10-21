0e13d443ac9b987e58d59cb0e6ea09c104ccd380
sarahthebest <sarah.emmoth@gmail.com>
2024-10-21 12:35:49 +0200

    landing och settings page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,19 @@
-import { CookieConsent, Header, ThemePicker } from "@daniellaalolo/fwk-23-4-components";
+import {
+    CookieConsent,
+    Header,
+    ThemePicker,
+    Footer,
+} from "@daniellaalolo/fwk-23-4-components";
 
 const Landing = () => {
     return (
         <div className="landing">
             <Header />
             <CookieConsent />
-            <ThemePicker onThemeChange={(themeName) => console.log(themeName)} />
+            <ThemePicker
+                onThemeChange={(themeName) => console.log(themeName)}
+            />
+            <Footer />
         </div>
     );
 };
--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -1,11 +1,18 @@
-import { Sidebar, ThemePicker } from "@daniellaalolo/fwk-23-4-components";
+import {
+    DataRequest,
+    Sidebar,
+    ThemePicker,
+} from "@daniellaalolo/fwk-23-4-components";
 
 const SettingsPage = () => {
     return (
         <div className="home flex-row">
             <Sidebar />
-            <div className="chatWrapper flex-col">
-            <ThemePicker onThemeChange={(themeName) => console.log(themeName)} />
+            <div className="settings flex-col">
+                <DataRequest />
+                <ThemePicker
+                    onThemeChange={(themeName) => console.log(themeName)}
+                />
             </div>
         </div>
     );
